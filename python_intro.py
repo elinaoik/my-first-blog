0812a3595cb25e7d2a2c0db4737e45dea2c52fb2
elinaoik <oikonomou.elin@gmail.com>
2018-03-24 17:31:57 +0100

    initial commit

---
+++ python_intro.py
@@ -0,0 +1,54 @@
+print('Hello, Django girls!')
+if 3>2:
+    print('It works!')
+
+if 5>2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
+
+name='Elina'
+if name == 'Ola':
+    print('Hey Ola')
+elif name == 'Elina':
+    print('Hey Elina')
+else:
+    print('Hey anonymous!')
+
+
+volume = 57
+if volume < 20:
+    print('It\'s kinda quiet.')
+elif 20 <= volume < 40:
+    print('It\'s nice for background music')
+elif 40 <= volume < 60:
+    print('Perfect, I can hear all the details')
+elif 60 <= volume < 80:
+    print('Nice for parties')
+elif 80 <= volume < 100:
+    print('A bit loud!')
+else:
+    print('My ears are hurting! :(')
+
+# Change the volume if it's too loud or too quiet
+
+def hi(name):
+    if name == 'Ola':
+        print('Hi Ola!')
+    elif name == 'Elina':
+        print('Hi Elina')
+    else:
+        print('Hi anonymous!')
+hi('Ola')
+
+def hithere(name):
+    print('Hi there '+name+'!')
+hithere('Marie')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Elina']
+for name in girls:
+    hithere(name)
+    print('Next girl')
+
+for i in range(1, 6):
+    print(i)    
